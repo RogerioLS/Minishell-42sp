2f387cd452b9e30ef75e9c2c01925f67a0c3eb84
Lucas <115382172+lucasl09@users.noreply.github.com>
2024-04-04 13:41:53 -0300

    Create line_var.c

---
+++ sources/variables/line_var.c
@@ -0,0 +1,14 @@
+/*
+** Verifica se a linha de comando é uma declaração de variável.
+** [PARÂMETROS]
+**#1. Os ponteiros para linha de comando (cmd);
+** [VALORES DE RETORNO]
+** Retorna verdadeiro se a linha de comando for uma declaração de variável e falso se não for.
+*/
+
+bool	commandline_var(char *cmd)
+{
+	if (ft_strchr(cmd, '=') == NULL)
+		return (false);
+	return (true);
+}

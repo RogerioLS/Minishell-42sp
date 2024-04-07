0ae75e1de2bd3fbd35b70c3125fb5837dd1a0e7b
Lucas <115382172+lucasl09@users.noreply.github.com>
2024-04-07 06:31:11 -0300

    Create exit_type.c

---
+++ sources/mandatory/variables/exit_type.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exit_type.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lluiz-de <lluiz-de@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/07 06:30:15 by lluiz-de          #+#    #+#             */
+/*   Updated: 2024/04/07 06:30:16 by lluiz-de         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//Colocar .h depois q montar
+
+int	ft_exit(char *buff)
+{
+	char	*arg;
+	int		x;
+
+	buff = ft_antispace(buff);
+	arg = ft_firstword(buff);
+	buff = ft_antispace(&buff[ft_strlen(arg)]);
+	ft_printf("exit\n");
+	if (ft_strlen(buff))
+	{
+		ft_printf("minishell: exit: too many arguments\n");
+		return (255);
+	}
+	x = 0;
+	g_vars->aftercmd = 0;
+	if (ft_strlen(arg) && ft_isdigit((int)arg[0]))
+		x = ft_atoi(arg);
+	else if (!ft_isdigit((int)arg[0]) && arg[0] != 0)
+	{
+		ft_printf("minishell: exit: %s: numeric argument required\n", arg);
+		x = 255;
+	}
+	kill(0, SIGQUIT);
+	exit(x);
+	return (0);
+}

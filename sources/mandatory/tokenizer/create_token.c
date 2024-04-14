f8a73c285058c7cb4cd34ed7e66a65354999ccd8
Lucas <115382172+lucasl09@users.noreply.github.com>
2024-04-13 23:34:31 -0300

    init create_token.c

---
+++ sources/mandatory/tokenizer/create_token.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   create_token.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lluiz-de <lluiz-de@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/13 23:33:24 by lluiz-de          #+#    #+#             */
+/*   Updated: 2024/04/13 23:34:02 by lluiz-de         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/mandatory/mini_shell.h"
+
+Token	*createToken(const char *text, TokenType type)
+{
+	Token	*token;
+
+	token = malloc(sizeof(Token));
+	if (!token)
+		return (NULL);
+	token->text = ft_strdup(text);
+	token->type = type;
+	token->next = NULL;
+	return (token);
+}
+
+void	freeTokens(Token *head)
+{
+	Token	*temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->text);
+		free(temp);
+	}
+}
+
+bool	isOperatorChar(char c)
+{
+	return (ft_strchr(" \t\n|&;()<>", c) != NULL);
+}

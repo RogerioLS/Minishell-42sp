23f0cc91a2d8ebee613be59a9dc9beb2cb708d69
RogLopes <roglopes@student.42sp.org.br>
2024-04-14 12:54:39 -0300

    add objects

--- sources/mandatory/main/main.c
+++ sources/mandatory/main/main.c
@@ -6,7 +6,7 @@
 /*   By: roglopes <roglopes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/13 15:27:05 by roglopes          #+#    #+#             */
-/*   Updated: 2024/04/13 19:08:36 by roglopes         ###   ########.fr       */
+/*   Updated: 2024/04/13 20:37:44 by roglopes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,15 @@ void	user_loop(char *cmd_line)
 	cmd_line = NULL;
 	while (1)
 	{
-		cmd_line = readline("mini_shell> ");
+		cmd_line = readline(":heart_eyes: mini_shell> ");
 		if (!cmd_line)
 			break ;
 		if (cmd_line && cmd_line)
 		{
 			add_history(cmd_line);
-			ft_antispace(cmd_line);
+			//ft_antispace(cmd_line);
 			//printf("Comando recebido: %s\n", cmd_line);
-			//ft_strtok(cmd_line, cmd_line);
+			ft_strtok(cmd_line, " ");
 		}
 		//printf("Comando recebido: %s\n", cmd_line);
 		free(cmd_line);

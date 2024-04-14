dfa0f2306f99d1afbd7cfc9367947b2147a1d76a
Lucas <115382172+lucasl09@users.noreply.github.com>
2024-04-14 01:24:25 -0300

    norminette adjust

--- sources/mandatory/tokenizer/tokenizer.c
+++ sources/mandatory/tokenizer/tokenizer.c
@@ -6,69 +6,34 @@
 /*   By: lluiz-de <lluiz-de@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/13 23:12:15 by lluiz-de          #+#    #+#             */
-/*   Updated: 2024/04/13 23:22:51 by lluiz-de         ###   ########.fr       */
+/*   Updated: 2024/04/14 01:18:46 by lluiz-de         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/mandatory/mini_shell.h"
 
-Token	*tokenizeInput(char *input)
+bool	is_operator_char(char c)
 {
-	Token	*head;
-	Token	**current;
-
-	head = NULL;
-	current = &head;
-	iterateTokens(input, current);
-	return (head);
+	return (ft_strchr(" \t\n|&;()<>", c) != NULL);
 }
 
-void	iterateTokens(char *input, Token **current)
-{
-	char	*start;
-	char	*cursor;
-
-	start = input;
-	cursor = input;
-	while (*cursor)
-	{
-		if (isInsideQuotes(cursor))
-			cursor = skipQuotes(cursor);
-		else if (isOperatorOrEnd(cursor))
-		{
-			if (start < cursor)
-			{
-				*current = createToken(start, TOKEN_WORD);
-				current = &(*current)->next;
-			}
-			if (isOperatorChar(*cursor))
-			{
-				*current = createToken(cursor, TOKEN_OPERATOR);
-				current = &(*current)->next;
-			}
-			start = cursor + 1;
-		}
-		cursor++;
-	}
-}
-
-bool	isInsideQuotes(char *cursor)
+bool	is_inside_quotes(char *cursor)
 {
 	return (*cursor == '\'' || *cursor == '\"');
 }
 
-char	*skipQuotes(char *cursor)
+char	*skip_quotes(char *cursor)
 {
-	char	quoteType;
+	char	quotetype;
 
-	quoteType = *cursor;
+	quotetype = *cursor;
 	cursor++;
-	while (*cursor && *cursor != quoteType)
+	while (*cursor && *cursor != quotetype)
 		cursor++;
 	return (cursor);
 }
 
-bool	isOperatorOrEnd(char *cursor)
+bool	is_operator_or_end(char *cursor)
 {
-	return (isOperatorChar(*cursor) || *(cursor + 1) == '\0');
+	return (is_operator_char(*cursor) || *(cursor + 1) == '\0');
 }

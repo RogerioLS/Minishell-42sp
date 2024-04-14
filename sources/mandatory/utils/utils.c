c4921fcda60e1871c004047b75f75519ed4bcaa1
Lucas <115382172+lucasl09@users.noreply.github.com>
2024-04-14 00:23:36 -0300

    Update utils.c

--- sources/mandatory/utils/utils.c
+++ sources/mandatory/utils/utils.c
@@ -54,16 +54,3 @@ char	*ft_firstword(char *buff)
 	aux[x] = '\0';
 	return (aux);
 }
-
-char	*ft_strchr(const char *s, int c)
-{
-	while (*s)
-	{
-		if (*s == c)
-			return ((char *)s);
-		s++;
-	}
-	if (c == '\0')
-		return ((char *)s);
-	return (NULL);
-}

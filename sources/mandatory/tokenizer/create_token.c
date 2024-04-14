aff0dac1087644a4eeca3ca5d443980ad4be1247
Lucas <115382172+lucasl09@users.noreply.github.com>
2024-04-14 01:23:49 -0300

    norminette adjust

--- sources/mandatory/tokenizer/create_token.c
+++ sources/mandatory/tokenizer/create_token.c
@@ -6,13 +6,13 @@
 /*   By: lluiz-de <lluiz-de@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/13 23:33:24 by lluiz-de          #+#    #+#             */
-/*   Updated: 2024/04/13 23:34:02 by lluiz-de         ###   ########.fr       */
+/*   Updated: 2024/04/14 01:17:33 by lluiz-de         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/mandatory/mini_shell.h"
 
-Token	*createToken(const char *text, TokenType type)
+Token	*create_token(const char *text, TokenType type)
 {
 	Token	*token;
 
@@ -25,7 +25,42 @@ Token	*createToken(const char *text, TokenType type)
 	return (token);
 }
 
-bool	isOperatorChar(char c)
+Token	*tokenize_input(char *input)
 {
-	return (ft_strchr(" \t\n|&;()<>", c) != NULL);
+	Token	*head;
+	Token	**current;
+
+	head = NULL;
+	current = &head;
+	iterate_tokens(input, current);
+	return (head);
+}
+
+void	iterate_tokens(char *input, Token **current)
+{
+	char	*start;
+	char	*cursor;
+
+	start = input;
+	cursor = input;
+	while (*cursor)
+	{
+		if (is_inside_quotes(cursor))
+			cursor = skip_quotes(cursor);
+		else if (is_operator_or_end(cursor))
+		{
+			if (start < cursor)
+			{
+				*current = create_token(start, TOKEN_WORD);
+				current = &(*current)->next;
+			}
+			if (is_operator_char(*cursor))
+			{
+				*current = create_token(cursor, TOKEN_OPERATOR);
+				current = &(*current)->next;
+			}
+			start = cursor + 1;
+		}
+		cursor++;
+	}
 }
